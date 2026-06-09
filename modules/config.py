af6c52a1a0510b87ea1ca91b37749a9cd69fa61f
Vivian Iyaha <90201480+vivianiyaha@users.noreply.github.com>
2026-06-09 16:51:50 +0100

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 def init_settings():
     if "company_name" not in st.session_state:
-        st.session_state.company_name = "Smart Payroll Ltd"
-        st.session_state.company_address = "Lagos, Nigeria"
+        st.session_state.company_name = "Cardstel Solutions Limited"
+        st.session_state.company_address = "11 Child Avenue, Apapa, Lagos, Nigeria"
         st.session_state.month = "June"
         st.session_state.year = "2026"
         st.session_state.duplicate_rule = "Sum Values"
